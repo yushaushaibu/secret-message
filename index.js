fc6e8d65d50b4e680171eada508eceaf05b40ad6
yushaushaibu <yushaushaibu1@gmail.com>
2022-11-26 07:44:43 +0000

    Encrypted string is attached to local URL and automatically selected

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.querySelector('form').addEventListener('submit', e => {
 
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
-    document.querySelector('#link-input').value = encrypted;
-});
 
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
+});
