e96785207ad4c3b9f8478ba0e12e3f894257c6d4
yushaushaibu <yushaushaibu1@gmail.com>
2022-11-26 07:57:31 +0000

    Element shows only URL link input when generate button is clicked

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
-
+    
     const input = document.querySelector('#message-input');
+    document.querySelector('#link-form').classList.remove('hide');
+
     const encrypted = btoa(input.value);
+    document.querySelector('#message-form').classList.add('hide');
 
     const linkInput = document.querySelector('#link-input');
     linkInput.value = `${window.location}#${encrypted}`;
