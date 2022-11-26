c6a6a8ad43be320f134572cb0c45cb3be46783a2
yushaushaibu <yushaushaibu1@gmail.com>
2022-11-26 08:07:17 +0000

    Decodes message

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const { hash } = window.location;
+console.log(atob(hash.replace('#', '')));
+
+
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
-    
+
     const input = document.querySelector('#message-input');
     document.querySelector('#link-form').classList.remove('hide');
 
